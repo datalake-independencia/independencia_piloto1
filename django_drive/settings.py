c5c332354d95960c5467ba4859d66f6a60687927
Municipalidad Independencia <75642182+datalake-independencia@users.noreply.github.com>
2021-01-18 15:29:56 -0300

    Update settings.py

--- django_drive/settings.py
+++ django_drive/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 AWS_ACCESS_KEY_ID = 'XXXXXXXXXXXXXXXXXXX'
 AWS_SECRET_ACCESS_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-AWS_STORAGE_BUCKET_NAME = 'independencia-bucket'
+AWS_STORAGE_BUCKET_NAME = 'independenciabucket'
 #AWS_S3_REGION_NAME = 'sa-east-1'
 #AWS_S3_ENDPOINT_URL = 'https://s3.sa-east-1.amazonaws.com'
 AWS_S3_FILE_OVERWRITE = False
